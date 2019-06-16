a61f4fc1dd599421e02e381ee01fd943349b0c4d
ykqfrost <yaokeqi1996@outlook.com>
2019-06-17 00:07:12 +0800

    init尝试

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>霜降</h1>
+        <ul>
+            <li>技术</li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    display: flex;
+    flex-direction: row;
+}
+
+</style>

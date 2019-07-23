6c60ee10c5397a1b62cf9b5282f7bc28d8b57acd
ykqfrost <yaokeqi1996@outlook.com>
2019-07-23 19:42:39 +0800

    文本格式确定

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制不使用分号结尾
+    semi: ['error', 'always', { 'omitLastInOneLineBlock': true }],
+    "space-before-function-paren": ["error", "never"]
   },
   parserOptions: {
     parser: 'babel-eslint'
